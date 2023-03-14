b9864d8bc8eefc1f779b7052b068c1fc7bcf28b7
Niranjan360 <connect2hemanthsimha@gmail.com>
2023-03-14 16:06:38 +0530

    rendering home as a public page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,11 @@
 import React from 'react'
-import { Navigate } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Dashboard from './Dashboard';
 
-function Home({user}) {
-    const isAuthenticated = () => {
-        console.log(user);
-        if(user!==null)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    };
+function Home() {
     return (
         <div>
-            {
-                isAuthenticated() ? 
-                (<Dashboard/>) : (<Navigate to="/login" replace/>)
-            }
+                <Dashboard/>
         </div>
     )
 }
